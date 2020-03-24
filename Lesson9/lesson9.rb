e203a3dbcde5e73f72c4e2c512ae25c77a550588
Suzy <suzy0131@gmail.com>
2020-03-23 22:19:50 -0700

    MadLib Assignment

---
+++ Lesson9/lesson9.rb
@@ -0,0 +1,32 @@
+x = 1
+puts x.class
+
+word = "hello"
+puts word.class
+
+x = 1
+x = x + 1
+print x
+
+x = 1
+y = x
+x = 3
+print y
+
+dog_name = "Rosa"
+cat_name = "Raquel"
+dog_age = 7
+cat_age = 11
+puts dog_age
+puts cat_name
+
+#run code in the terminal
+puts "What's your name?"
+user_input = gets
+name = user_input.chomp
+
+#Cinverting uset input
+puts "2".to_i
+
+# rest practice finished by terminal
+
